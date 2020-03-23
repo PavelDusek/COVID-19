924f5e827176466ea31dbbc47ea711bfdde42c01
Pavel Dusek <pavel.dusek@gmail.com>
2020-03-23 21:44:49 +0100

    added prediction based on last week model, added estimation of real cases

--- predict.py
+++ predict.py
@@ -10,7 +10,21 @@ import json
 import os.path
 
 
+
+def exponential_model( data ):
+    model = smf.ols( 'log ~ index', data=data )
+    result = model.fit()
+    params = {
+            'index': result.params['index'],
+            'Intercept': result.params['Intercept'],
+            'R2': result.rsquared_adj,
+            'p_index': result.pvalues['index'],
+            'p_Intercept': result.pvalues['Intercept'],
+            }
+    return ( result, params )
+
 params = {}
+plt_colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 if os.path.isfile("params.json"):
     with open("params.json") as f:
         json_params = f.read()
@@ -34,21 +48,18 @@ cz.loc[:, 'cumsum'] = cz.sort_values(by='DateRep', ascending=True)['Cases'].cums
 cz.loc[:, 'log'] = np.log( cz['cumsum'] )
 
 model_data = cz.loc[ cz['log'] > 0]
-model = smf.ols( 'log ~ index', data=model_data )
-result = model.fit()
-params[today] = {
-        'index': result.params['index'],
-        'Intercept': result.params['Intercept'],
-        'R2': result.rsquared_adj,
-        'p_index': result.pvalues['index'],
-        'p_Intercept': result.pvalues['Intercept'],
-        }
+result, params[today] = exponential_model( data=model_data )
+model_data_last_week = model_data.loc[ model_data['DateRep'] >= ( datetime.datetime.now() + datetime.timedelta(days=-8) ) ]
+result_last_week, params[f"{today}_for_last_week"] = exponential_model( data=model_data_last_week )
 
 f, ax = plt.subplots( nrows=1, ncols=2 )
 ax[0].plot( model_data['index'], model_data['log'], label='Real Data')
 ax[0].plot( model_data['index'], result.predict(model_data['index']), label='Model' )
-ax[0].text( model_data['index'][0], model_data['log'].max() - 0.5, 'R^2 = {:.2f}'.format( result.rsquared_adj ) )
-ax[0].text( model_data['index'][0], model_data['log'].max() - 1, 'Cases = e^( {:.3f} * day + {:.3f} )'.format( result.params['index'], result.params['Intercept'] ) )
+ax[0].plot( model_data_last_week['index'], result_last_week.predict(model_data_last_week['index']), label='Model for the last week' )
+ax[0].text( model_data['index'][0], model_data['log'].max() - 0.5, 'R^2 = {:.2f}'.format( result.rsquared_adj ), c=plt_colors[1] )
+ax[0].text( model_data['index'][0], model_data['log'].max() - 0.75, 'Cases = e^( {:.3f} * day + {:.3f} )'.format( result.params['index'], result.params['Intercept'] ), c=plt_colors[1])
+ax[0].text( model_data['index'][0], model_data['log'].max() - 1, 'R^2 = {:.2f}'.format( result_last_week.rsquared_adj ), c=plt_colors[2] )
+ax[0].text( model_data['index'][0], model_data['log'].max() - 1.25, 'Cases = e^( {:.3f} * day + {:.3f} )'.format( result_last_week.params['index'], result_last_week.params['Intercept'] ), c=plt_colors[2])
 ax[0].set_ylabel("Log No. cases")
 ax[0].legend()
 
@@ -74,7 +85,9 @@ ax[1].plot( cz['DateRep'], cz['cumsum'], label='Real Data')
 
 ### Just one model ###
 cz[f'model'] = np.exp( result.params['index'] * cz['index'] + result.params['Intercept'] )
+cz[f'model_last_week'] = np.exp( result_last_week.params['index'] * cz['index'] + result_last_week.params['Intercept'] )
 ax[1].plot( cz['DateRep'], cz[f'model'], label=f'Model')
+ax[1].plot( cz['DateRep'], cz[f'model_last_week'], label=f'Model for last week data')
 ### Model by dates ###
 ## All dates ##
 #for day in params.keys():
@@ -92,3 +105,50 @@ plt.suptitle(f"COVID-19 cases, Czech Republic, data from ecdc.europa.eu as of {t
 plt.show()
 
 with open("params.json", "w") as f: f.write( json.dumps( params, indent=4, sort_keys=True ) )
+
+
+########################################################
+# Estimation of Infected Cases based on Mortality Data #
+########################################################
+# Method described here: https://medium.com/@tomaspueyo/coronavirus-act-today-or-people-will-die-f4d3d9cd99ca
+
+def round_date( date ):
+    year = date.year
+    month = date.month
+    day = date.day
+    if date.hour > 12: day = day + 1
+    return datetime.datetime( year, month, day )
+
+# Data from https://medium.com/@tomaspueyo/coronavirus-act-today-or-people-will-die-f4d3d9cd99ca
+mortality_rate_mean = 0.034
+mortality_rate_upper = 0.05
+mortality_rate_lower = 0.005
+
+last_date = max( cz.loc[ cz['Deaths'] == cz['Deaths'].max(), 'DateRep'])
+deaths = cz.loc[ cz['DateRep'] == last_date, 'Deaths']
+deaths = deaths.values[0]
+estimated_date = last_date + datetime.timedelta(days=-17.2)
+
+calculated_date = estimated_date
+mean_cases = deaths / mortality_rate_mean
+max_cases  = deaths / mortality_rate_lower
+min_cases  = deaths / mortality_rate_upper
+
+previous_date = estimated_date + datetime.timedelta(days=-6.2)
+estim = pd.DataFrame(columns=['Date', 'Estim_Cases', 'Max_Estim_Cases', 'Min_Estim_Cases'])
+estim.loc[ 0 ] = [ previous_date, mean_cases/2, max_cases/2, min_cases/2 ]
+while( calculated_date <= last_date ):
+    estim.loc[ estim.shape[0] ] = [ calculated_date, mean_cases, max_cases, min_cases ]
+    calculated_date = calculated_date + datetime.timedelta(days=+6.2)
+    mean_cases = mean_cases * 2
+    max_cases = max_cases * 2
+    min_cases = min_cases * 2
+estim.loc[ estim.shape[0] ] = [ calculated_date, mean_cases, max_cases, min_cases ]
+f = plt.figure()
+plt.plot('DateRep', 'cumsum', data=cz[['DateRep', 'cumsum']].dropna(), label='Confirmed Cases')
+#plt.plot('Date', 'Estim_Cases', data=estim[['Date', 'Estim_Cases']])
+plt.fill_between( 'Date', 'Min_Estim_Cases', 'Max_Estim_Cases', alpha=0.2, data=estim, label='Estimated Cases')
+plt.suptitle(f"COVID-19 cases, Czech Republic, data from ecdc.europa.eu as of {today}\nEstimation of Real Cases, Based on Mortality Data,\ncode at https://github.com/PavelDusek/COVID-19")
+plt.xticks(rotation=25)
+plt.legend()
+plt.show()
